6f5f3a4d1a1e799ddda0d43af788325fc552d267
atec_khkim <khsyee@gmail.com>
2017-09-28 16:28:53 +0900

    second

--- photos/models.py
+++ photos/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-class Photo(model.Model):
-    id = 'index'
-    image = 'origin_image'
-    filtered_image = 'filtered_image'
-    content = '사진에 대한 설명문'
-    created_at = '생성일시'
+class Photo(models.Model):
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/orig') # original image
+    filtered_image = models.ImageField(upload_to='uploads/%Y/%m/%d/filtered') # modified image
+    content = models.TextField(max_length=500, null=True, blank=True) # text
+    created_at = models.DateTimeField(auto_now_add=True) # created datetime
